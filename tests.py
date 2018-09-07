abddb3b9de06dddc789e9fcc643b87b1459bb9b5
Joel Hillacre <joel@403forbidden.ca>
2018-09-07 12:31:20 -0600

    fix coverage of tests.

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2018 Arrai Innovations Inc. - All Rights Reserved
+import json
 import os
 import subprocess
 import venv
@@ -20,32 +21,48 @@ class ExtraneousTestCase(TestCase):
         cls.cwd_path = cls._cwd_path.__enter__()
         cls.env_path = cls._env_path.__enter__()
         cls.setup_venv()
+        cls.subcmd('coverage erase', cwd_path=os.getcwd(), parent_envs=True)
 
     @classmethod
     def tearDownClass(cls):
+        cls.subcmd('cp {cwd_path}/.coverage.* {real_cwd}/'.format(cwd_path=cls.cwd_path, real_cwd=os.getcwd()))
+        cls.subcmd('coverage combine', cwd_path=os.getcwd(), parent_envs=True)
+        try:
+            cls.subcmd('rm -rf htmlcov', cwd_path=os.getcwd(), parent_envs=True)
+        except subprocess.CalledProcessError:
+            pass
+        cls.subcmd('coverage html', cwd_path=os.getcwd(), parent_envs=True)
         cls._env_path.__exit__(None, None, None)
         cls._cwd_path.__exit__(None, None, None)
 
-    @staticmethod
-    def subcmd(cmd, cwd_path=None):
+    @classmethod
+    def subcmd(cls, cmd, cwd_path=None, coverage=False, parent_envs=False):
         kwargs = {
-            'shell': True, 'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE, 'check': False
+            'shell': True,
+            'stdout': subprocess.PIPE,
+            'stderr': subprocess.PIPE,
+            'check': False,
+            'cwd': cls.cwd_path if not cwd_path else cwd_path
         }
-        if cwd_path:
-            kwargs['cwd'] = cwd_path
+        if not parent_envs:
+            kwargs['env'] = cls.env_vars
+        if coverage:
+            cmd = 'coverage run -p ' + cmd
         ran = subprocess.run(cmd, **kwargs)
         try:
             ran.check_returncode()
         except subprocess.CalledProcessError as e:
-            print('stdout', ran.stdout)
-            print('stderr', ran.stderr)
+            if ran.stdout:
+                print('stdout', ran.stdout)
+            if ran.stderr:
+                print('stderr', ran.stderr)
             raise
         return ran
 
     @classmethod
     def pip_install(cls, package, editable=False, upgrade=False):
         return cls.subcmd(
-            '{env_path}/bin/python -m pip install {upgrade}{editable}{package}'.format(
+            'python -m pip install {upgrade}{editable}{package}'.format(
                 env_path=cls.env_path,
                 upgrade='--upgrade ' if upgrade else '',
                 editable='-e ' if editable else '',
@@ -57,37 +74,54 @@ class ExtraneousTestCase(TestCase):
     def setup_venv(cls, editable=False):
         real_cwd = os.getcwd()
         venv.create(cls.env_path, with_pip=True)
-        cls.pip_install('pip setuptools', upgrade=True)
+        venv_vars = subprocess.run(
+            'deactivate; source {env_path}/bin/activate; jq -n -M env'.format(env_path=cls.env_path),
+            **{'shell': True, 'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE, 'check': True}
+        ).stdout
+        cls.env_vars = json.loads(venv_vars)
+        cls.env_vars.pop('PYTHONPATH')
+        cls.pip_install('pip setuptools coverage', upgrade=True)
         cls.pip_install(real_cwd, editable=True)
-        for package in [
+        cls.subcmd('echo "import coverage; coverage.process_startup()" > `{env_path}/bin/python -c "import sys; print([x for x in sys.path if \'site-packages\' in x][0] + \'/coverage-all-the-things.pth\')"`'.format(
+            env_path=cls.env_path
+        ))
+        cls.pip_install(' '.join('{}/test_packages/{}'.format(real_cwd, package) for package in [
             'extraneous_sub_package_1',
             'extraneous_sub_package_2',
             'extraneous_top_package_1',
             'extraneous_top_package_2',
             'extraneous_top_package_3',
-        ]:
-            pkg_name = '{}/test_packages/{}'.format(real_cwd, package)
-            cls.pip_install(pkg_name, editable=editable)
+        ]), editable=editable)
         with open('{cwd_path}/requirements.txt'.format(cwd_path=cls.cwd_path), mode='w') as w:
             w.write('extraneous-top-package-1\n')
         with open('{cwd_path}/test_requirements.txt'.format(cwd_path=cls.cwd_path), mode='w') as w:
-            w.write('extraneous-top-package-3\n')
+            w.write('extraneous-top-package-3\ncoverage\n')
+        with open('{cwd_path}/.coveragerc'.format(cwd_path=cls.cwd_path), mode='w') as w:
+            w.write('''[run]
+branch = True
+parallel = True
+concurrency = multiprocessing
+include = *extraneous/extraneous.py
+
+[report]
+exclude_lines =
+    raise NotImplementedError
+    except ImportError'''.format(env_path=cls.env_path))
 
     @classmethod
     def get_sitepackages_for_venv(cls):
         ran = cls.subcmd(
-            '{env_path}/bin/python -c "from site import getsitepackages; import os;'
+            'python -c "from site import getsitepackages; import os;'
             'print(\'\\n\\t\'.join([os.path.relpath(x, os.getcwd()) for x in getsitepackages()]))"'.format(
                 env_path=cls.env_path
-            ),
-            cwd_path=cls.cwd_path
+            )
         )
         return ran.stdout.decode('utf8').strip()
 
     def test_verbose(self):
         extraneous = self.subcmd(
             '{env_path}/bin/extraneous.py -v'.format(env_path=self.env_path),
-            cwd_path=self.cwd_path
+            coverage=True
         )
         self.assertMultiLineEqual(
             'reading installed from:\n\t{site_packages}\n'
@@ -118,19 +152,19 @@ class ExtraneousTestCase(TestCase):
     def test_full(self):
         extraneous = self.subcmd(
             '{env_path}/bin/extraneous.py -f'.format(env_path=self.env_path),
-            cwd_path=self.cwd_path
+            coverage=True
         )
         self.assertMultiLineEqual(
             '{extraneous}\n'
             'uninstall via:\n\tpip uninstall -y {uninstall}\n'.format(
                 extraneous=color(
                     'extraneous packages:\n\t{}'.format(' '.join(sorted({
-                        'extraneous-top-package-2', 'extraneous'
+                        'extraneous-top-package-2', 'extraneous', 'setuptools'
                     }))),
                     fg='yellow'
                 ),
                 uninstall=' '.join(sorted({
-                    'extraneous-top-package-2', 'extraneous'
+                    'extraneous-top-package-2', 'extraneous', 'setuptools'
                 }) + sorted({
                     'extraneous-sub-package-2', 'ansicolors', 'pipdeptree'
                 })),
@@ -141,7 +175,7 @@ class ExtraneousTestCase(TestCase):
     def test_exclude_top(self):
         extraneous = self.subcmd(
             '{env_path}/bin/extraneous.py -e extraneous-top-package-2'.format(env_path=self.env_path),
-            cwd_path=self.cwd_path
+            coverage=True
         )
         self.assertMultiLineEqual(
             '',
@@ -151,7 +185,7 @@ class ExtraneousTestCase(TestCase):
     def test_exclude_sub(self):
         extraneous = self.subcmd(
             '{env_path}/bin/extraneous.py -e extraneous-sub-package-2'.format(env_path=self.env_path),
-            cwd_path=self.cwd_path
+            coverage=True
         )
         self.assertMultiLineEqual(
             '{extraneous}\n'
@@ -172,14 +206,14 @@ class ExtraneousTestCase(TestCase):
 
     def test_include(self):
         other_req = NamedTemporaryFile(mode='w+', delete=False)
-        other_req.write('extraneous-top-package-2\n')
+        other_req.write('extraneous-top-package-2\ncoverage\n')
         other_req.close()
         try:
             extraneous = self.subcmd(
                 '{env_path}/bin/extraneous.py -v -i {other_req}'.format(
                     env_path=self.env_path, other_req=other_req.name
                 ),
-                cwd_path=self.cwd_path
+                coverage=True
             )
             self.assertMultiLineEqual(
                 'reading installed from:\n\t{site_packages}\n'
